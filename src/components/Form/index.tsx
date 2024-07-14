25978942ad490ac693a1fc96f054a2c412f9057f
dimasneto7 <dimas_fcn@outlook.com>
2024-07-14 18:13:13 -0300

    task

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { ITask } from '../../types/task'
+import Button from '../Button'
+import style from './Form.module.scss'
+import { v4 as uuidv4 } from 'uuid'
+
+class Form extends React.Component<{
+  setTasks: React.Dispatch<React.SetStateAction<ITask[]>>
+}> {
+  state = {
+    task: '',
+    time: '00:00',
+  }
+
+  addTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    this.props.setTasks((oldTasks) => [
+      ...oldTasks,
+      { ...this.state, selected: false, completed: false, id: uuidv4() },
+    ])
+    this.setState({
+      task: '',
+      time: '00:00',
+    })
+  }
+
+  render() {
+    return (
+      <form className={style.novaTarefa} onSubmit={this.addTask.bind(this)}>
+        <div className={style.inputContainer}>
+          <label htmlFor="task">Adicione um novo estudo</label>
+          <input
+            type="text"
+            name="task"
+            id="task"
+            value={this.state.task}
+            onChange={(e) =>
+              this.setState({ ...this.state, task: e.target.value })
+            }
+            placeholder="O que você quer estudar?"
+            required
+          />
+        </div>
+
+        <div className={style.inputContainer}>
+          <label htmlFor="time">Tempo</label>
+          <input
+            type="time"
+            step="1"
+            name="time"
+            value={this.state.time}
+            onChange={(e) =>
+              this.setState({ ...this.state, time: e.target.value })
+            }
+            id="time"
+            min="00:00:00"
+            max="01:30:00"
+            required
+          />
+        </div>
+
+        <Button type="submit">Adicionar</Button>
+      </form>
+    )
+  }
+}
+
+export default Form
---
+++ src/pages/App.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import Form from '../components/Form'
+import List from '../components/List'
+import Timer from '../components/Timer'
+import { ITask } from '../types/task'
+import style from './style.module.scss'
+
+function App() {
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  return (
+    <div className={style.AppStyle}>
+      <Form setTasks={setTasks} />
+      <List tasks={tasks} />
+      <Timer />
+    </div>
+  )
+}
+
+export default App
